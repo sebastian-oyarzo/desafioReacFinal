95975574dd0d96f5e43025018df8b8e05b04b440
sebastian <sebastian.oyarzo.zapata@gmail.com>
2024-05-11 16:05:05 -0400

    renderizacion api

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,19 +4,17 @@ export const Buscador = () => {
   return (
     <div className='Buscador border'>
             <select name="TipoDeFiltro" id="categorias" className='m-3 fs-3 text-center p-2'>
-                <option value="1">tamaño</option>
-                <option value="2">cantidad de habitantes</option>
-                <option value="3">por letra Inicial</option>
+                <option value="1">con mas apariciones en los comics</option>
+                <option value="2">con mas eventos importantes</option>
             </select>
-            <input type="text" placeholder='buscar' className='m-3  fs-4 p-2' />
+            <input type="text" placeholder='buscar por nombre' className='m-3  fs-4 p-2' />
             <div>
                 <button className='m-3 border fs-3'>buscar</button>
             </div>
             <select name="TipoDeFiltro2" id="categorias2" className='m-3 fs-3 text-center p-2'>
-                <option value="4">paises que hablan ingles</option>
-                <option value="5">paises que hablan español</option>
+                <option value="4">ascendente A-Z</option>
+                <option value="5">descendente Z-A</option>
             </select>
-            <button className='border m-3 border fs-3'>busca por idioma</button>
     </div>
   )
 }
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import { Collapse } from 'react-bootstrap'
+
+
+// URL? = https://gateway.marvel.com:443/v1/public/characters?apikey=b222c4b71c5065a43c5a75feb85997da
+// llave publica: b222c4b71c5065a43c5a75feb85997da
+// llave privada: 639f6fb8c0910db576804959ec806e3f3855b2c0
+// ts: 1 (podria ser cualquier numero)
+
+//hash: ts=1 + llave privada + llave publica =  1639f6fb8c0910db576804959ec806e3f3855b2c0b222c4b71c5065a43c5a75feb85997da
+//hash decodificado: 00d8434c0524e7ecd0800c11a4cf00d5
+
+
+
+// con ts=1
+//https://gateway.marvel.com:443/v1/public/comics?ts=1&format=comic&formatType=comic&noVariants=true&dateRange=1980%2C2020&orderBy=onsaleDate&apikey=b222c4b71c5065a43c5a75feb85997da&hash=00d8434c0524e7ecd0800c11a4cf00d5
+
+// con ts=2
+//https://gateway.marvel.com:443/v1/public/comics?ts=2&format=comic&formatType=comic&noVariants=true&dateRange=1980%2C2020&orderBy=onsaleDate&apikey=b222c4b71c5065a43c5a75feb85997da&hash=fc2d4e1703140fc7746066f7db8cc27d
+
+// con ts=1 pero sin los filtros
+// https://gateway.marvel.com:443/v1/public/comics?ts=1&format=comic&formatType=comic&noVariants=true&orderBy=onsaleDate&apikey=b222c4b71c5065a43c5a75feb85997da&hash=00d8434c0524e7ecd0800c11a4cf00d5
+
+//https://gateway.marvel.com:443/v1/public/series?ts=1&apikey=b222c4b71c5065a43c5a75feb85997da&hash=00d8434c0524e7ecd0800c11a4cf00d5
+
 
 export const MiApi = () => {
   const [Personajes, setPersonajes] = useState([])
@@ -10,25 +38,53 @@ export const MiApi = () => {
 
       const allFiltered = response.data.data.results.map((nombre) =>{
         const nombreFiltrado = {
-          arrayNames: nombre.name,
+          nombrePersonaje: nombre.name,
           descripcion: nombre.description,
           imagenes: nombre.thumbnail.path,
           aparicionesComic: nombre.comics.available,
-          eventosdondeactua: nombre.events.available,
+          eventosDondeActua: nombre.events.available,
         }
         return nombreFiltrado
       })
 
-    console.log(allFiltered)
+    setPersonajes(allFiltered)
 
   }
-
 
   useEffect(() => {
     dataFromApi()
   },[])
-
+  console.log(Personajes)
   return (
-    <div><h1>estoy pasando</h1></div>
+        <Row xs={1} md={3} className="g-4 mt-3">
+            {Personajes.length ?  Personajes.map((personaje, key) => (
+                <Col key={key} md={{ span: 3, offset: 2 }}>
+                      <Card  >
+                    <Card.Img variant="top" src={personaje.imagenes + ".jpg"} />
+                    <Card.Body>
+                      <Card.Title>{personaje.nombrePersonaje}</Card.Title>
+                      <Card.Text>
+                        {personaje.descripcion}
+                      </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                      <ListGroup.Item>apariciones en los comic: {personaje.aparicionesComic}</ListGroup.Item>
+                      <ListGroup.Item>eventos importantes en los que actua: {personaje.eventosDondeActua}</ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body>
+                      <Card.Link href="#">Card Link</Card.Link>
+                      <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                  </Card>
+              
+              
+              
+              </Col>
+
+                  ))
+                : (<p>sadasd</p>)
+
+            }
+          </Row>
   )
 }
